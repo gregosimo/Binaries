42edb84ae432c0f0743ecb955d1fbe07af36c753
Gregory Simonian <simonian@astronomy.ohio-state.edu>
2016-11-18 12:27:55 -0500

    Functions to easily make HR diagrams.

---
+++ hrplots.py
@@ -0,0 +1,34 @@
+"""Script which takes care of setting up plots in various HR-like diagrams."""
+
+import matplotlib.pyplot as plt
+
+def logg_teff_plot(teff, logg, style="k.", **kwargs):
+    '''Creates a plot in the Teff-logg space.
+
+    Teff and logg are valuse which should be plotted. Style should be the plot
+    style for the points; they are black dots by default. All other values will
+    be passed to the underlying errorbar routine.
+    '''
+    plt.errorbar(teff, logg, fmt=style, **kwargs)
+    invert_x_axis()
+    invert_y_axis()
+    plt.xlabel("Teff")
+    plt.ylabel("log g")
+
+def invert_x_axis(axes=None):
+    '''Inverts the x-axis for given axes (useful for Teff)'''
+    if axes is None:
+        axes = plt.gca()
+    xlims = axes.get_xlim()
+    if xlims[0] < xlims[1]:
+        # Invert the axis
+        axes.set_xlim(xlims[::-1])
+        
+def invert_y_axis(axes=None):
+    '''Inverts the y-axis for given axes (useful for log g)'''
+    if axes is None:
+        axes = plt.gca()
+    ylims = axes.get_ylim()
+    if ylims[0] < ylims[1]:
+        # Invert the axis
+        axes.set_ylim(ylims[::-1])
