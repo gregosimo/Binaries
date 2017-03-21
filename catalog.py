05b3da064142010ce37b4cd3020fa3da7db6c10d
Gregory Simonian <simonian@astronomy.ohio-state.edu>
2017-03-21 17:30:44 -0400

    Added a function to compare the KIC radii to the radii inferred from DSEP.

--- catalog.py
+++ catalog.py
@@ -1655,6 +1655,24 @@ def DSEP_dwarf_radii(teffs, metallicities, alphas, age=2.0, lowTeff=3000,
 
     return model_radii
 
+def compare_DSEP_radii_to_KIC_radii(
+    kic_radii, DSEP_radii, kic_radii_err_high, kic_radii_err_low, teff):
+    '''Plot the fractional difference between DSEP radii and KIc radii.
+
+    Calculate the fractional difference between the radii of the KIC and that
+    calculated by DSEP. This will include the uncertainties of the KIC
+    radius.'''
+    frac = (kic_radii - DSEP_radii) / DSEP_radii
+    fracerrs = [kic_radii_err_low / DSEP_radii, 
+                kic_radii_err_high / DSEP_radii]
+
+    print(frac)
+    print(fracerrs)
+
+    plt.errorbar(teff, frac, yerr=fracerrs, marker='o', color="b", ls="")
+    hr.invert_x_axis()
+    plt.xlabel("APOGEE Teff (K)")
+    plt.ylabel("Fractional KIC - DSEP radius")
 
 def HR_standout_plot(
     fullsample, rv_nonvar, rv_var, Teff_colname="TEFF_FIT",
