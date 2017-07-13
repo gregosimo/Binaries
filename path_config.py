b7717b02defb5c818329908026d12993147610d1
Gregory Simonian <simonian@astronomy.ohio-state.edu>
2017-07-13 17:11:39 -0400

    Updated the APOKASC catalog.

--- path_config.py
+++ path_config.py
@@ -28,7 +28,7 @@ M67_TWOMASS = M67_PATH / "2mass_photometry.ipac"
 M67_EPIC = M67_PATH / "M67_WOCS_EPIC_match.txt"
 
 MCQUILLAN_CATALOG = HEAD_DIR / "McQuillan.fit"
-APOKASC_PATH = HEAD_DIR / "APOKASC_cat_v3.6.5.fits"
+APOKASC_PATH = HEAD_DIR / "APOKASC_cat_v4.2.3.fits"
 KIC_PULSATORS = HEAD_DIR / "pulsators.kic"
 UKIRT_RESULTS = HEAD_DIR / "ukirt_results.csv.gz"
 RAFA_SAVITA_PERIODS = HEAD_DIR / "Prot_OK_noCP.txt"
