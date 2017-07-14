f7971257ff6b6b0dc23aff3e222b75846cd533a2
Gregory Simonian <simonian@astronomy.ohio-state.edu>
2017-07-14 11:09:04 -0400

    Added a function to read in a table with DR14 McQuillan targets with KIC parameters.

--- read_catalog.py
+++ read_catalog.py
@@ -342,6 +342,20 @@ def mcquillan_dr14_overlap(
     mcq_dr14 = catalog.join_by_2MASS_key(mcq_col, dr14, "tm_designation", "APOGEE_ID")
     return mcq_dr14
 
+@au.shortcut_file(paths.SHORTCUT_MCQUILLAN_DR14_KIC)
+def mcquillan_dr14_overlap_with_stelparms(
+    mcq_path=paths.MCQUILLAN_CATALOG, apopath=paths.DR14_ALLSTAR_PATH,
+    kicpath=paths.KIC_CATALOG):
+    '''Read in the McQuillan/APOGEE overlap with KIC parameters.'''
+    dr14_stelparms = dr14_with_KIC_stelparms(apopath, kicpath)
+    mcq = read_McQuillan_catalog(mcq_path)
+    del(mcq["Teff"])
+    del(mcq["log_g_"])
+    del(mcq["Mass"])
+    fullcat = au.join_by_id(mcq, dr14_stelparms, "KIC", "kepid")
+    return fullcat
+
+
 @au.shortcut_file(paths.SHORTCUT_MCQUILLAN_EHK)
 def mcquillan_photometry(
     mcq_path=paths.MCQUILLAN_CATALOG, photo_path=paths.EHK_PATH):
