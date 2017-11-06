b57b91c620eabec9f607c17c6cc8c6fc57c116ac
Gregory Simonian <simonian@astronomy.ohio-state.edu>
2017-11-06 11:26:42 -0500

    File to export tables and figures necessary for the paper.

--- paperexport.py
+++ paperexport.py
@@ -22,20 +22,25 @@ def build_filepath(toplevel, filename, suffix="png"):
     fullpath = toplevel / ".".join((filename, suffix))
     return str(fullpath)
 
+def get_sample():
+    '''Get the final sample of the observing targets.'''
+    final_sample = Table.read(
+        paths.HEAD_DIR, "Obs_list.txt", format="ascii.fixed_width")
+    return final_sample
+
 def create_observing_sample_table(
         dest=build_filepath(TABLE_PATH, "obsprops", "tex")):
     '''Create the observing table with relevant parameters.
 
     For this table, the relevant parameters are the Huber Teff and log(g), the
     McQuillan period, and the EHK V-band magnitude.'''
-    init_sample = obs.select_observing_targets(30)
-    final_sample = catalog.perform_period_cut(init_sample, lowperiod=1.1)
+    sample = get_sample()
 
-    sample_names = final_sample["KIC_A"]
-    sample_teff = final_sample["teff"]
-    sample_logg = final_sample["logg"]
-    sample_period = final_sample["Prot"]
-    sample_V = final_sample["V"]
+    sample_names = sample["KIC"]
+    sample_teff = sample["teff"]
+    sample_logg = sample["logg"]
+    sample_period = sample["Prot"]
+    sample_V = sample["V"]
 
     endcomments = r"""
 For each Kepler target, the \(T_{eff}\) and \(\log (g)\) values are taken from 
@@ -61,6 +66,37 @@ of the Kepler field."""
 
     output_table.write(dest, format="latex", latexdict=obsdict,
                        formats=column_format)
+
+
+def create_APOGEE_table(dest=build_filepath(TABLE_PATH, "apotab", "tex")):
+    '''Create the table showing APOGEE parameters for objects that have them.
+
+    This table will contain APOGEE-determined Teff, logg, vsini, and
+    vscatter.'''
+    sample = get_sample()
+
+    kic_name = sample["KIC"]
+    apo_apid = sample["APOGEE_ID"]
+    apo_teff = sample["TEFF"]
+    apo_logg = sample["FPARAM"][:,1]
+    apo_vsini = sample["VSINI"]
+    apo_vscatter = sample["VSCATTER"]
+
+    endcomments = r"""APOGEE parameters are from DR14. \log (g) are uncorrected
+    \log(g) values from FERRE."""
+
+    obsdict = {"tabletype": "table*", "tablealign": "htb", 
+               "col_align": "r r c c c c", 
+               "caption": "Targets with APOGEE observations\\label{tab:apogee}",
+               "preamble": "", "header_start": "", "data_start": "\\tableline",
+               "data_end": "", "tablefoot": endcomments
+               }
+
+    output_table = Table(
+        [kic_name, apo_apid, apo_teff, apo_logg, apo_vsini, apo_vscatter],
+        names=("KIC", r"APOGEE \(T_{eff}\)", r"APOGEE \(\log (g)\)", 
+               r"v \sin i", "VScatter")) 
+    column_format = {r"\(\log (g)\)": ".2f", r"\(P_{rot}\)": ".2f", "V": ".1f"}
 
 def create_sample_HR_diagram(dest=build_filepath(FIGURE_PATH, "sample")):
     '''HR diagram showing the location of the observing sample.
