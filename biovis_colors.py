d72fba5c9ed24a7629b9f746985884872b9da33a
Gregory Simonian <simonian@astronomy.ohio-state.edu>
2018-02-03 10:47:22 -0500

    Added color-blind color palette module.

---
+++ biovis_colors.py
@@ -0,0 +1,27 @@
+'''A package implementing the 15 color palette for color blindness from Martin
+Krzywinsky. The original web page can be found here:
+http://mkweb.bcgsc.ca/biovis2012/'''
+
+colordict = {
+    "black": (0, 0, 0), "pink": (255/255.0, 109/255.0, 182/255.0), 
+    "purple": (73/255.0, 0, 146/255.0), "blue": (0, 109/255.0, 219/255.0), 
+    "violet": (182/255.0, 109/255.0, 255/255.0), 
+    "sky blue": (109/255.0, 182/255.0, 255/255.0), "red": (146/255.0, 0, 0),
+    "brown": (146/255.0, 73/255.0, 0), "orange": (219/255.0, 209/255.0, 0),
+    "green": (36/255.0, 255/255.0, 36/255.0), 
+    "yellow": (255/255.0, 255/255.0, 109/255.0)}
+
+black = colordict["black"]
+pink = colordict["pink"]
+purple = colordict["purple"]
+blue = colordict["blue"]
+violet = colordict["violet"]
+sky_blue = colordict["sky blue"]
+red = colordict["red"]
+brown = colordict["brown"]
+orange = colordict["orange"]
+green = colordict["green"]
+yellow = colordict["yellow"]
+
+
+
