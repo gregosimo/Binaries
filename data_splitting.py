42939851e3657b1117817b593ede736e38493df6
Gregory Simonian <simonian@astronomy.ohio-state.edu>
2018-02-12 16:34:18 -0500

    Split based on whether subgiants/dwarfs lie above or below a diagonal line.

--- data_splitting.py
+++ data_splitting.py
@@ -821,6 +821,37 @@ class APOGEESplitter(KeplerSplitter):
 
         self._setup_complement_index(splitnames, full_sample, cool_crit)
 
+    def split_evstate(
+        self, teff_col="TEFF", logg_col="LOGG_FIT", 
+        topdiv_slope=0, topdiv_coord=(5000, 3.5), 
+        bottomdiv_slope=(5690-4640)/(4.43-3.72), bottomdiv_coord=(4640, 3.72), 
+        splitnames=("Giant", "Subgiant", "Dwarf"), 
+        crit="APOGEE Evolutionary State"):
+        '''Split the cool dwarf sample by evolutionary state.
+
+        This function splits the sample according to linear cuts. There is a
+        linear cut between giants and subgiants, and a linear cut between
+        subgiants and dwarfs.
+
+        The default cut between giants and subgiants is a horizontal log(g) 
+        cut of 3.5. The default cut between subgiants and dwarfs is a linear
+        cut calibrated to an APOGEE HR diagram.
+        '''
+        giant_indices = (self.data[logg_col] < topdiv_slope * (
+            self.data[teff_col] - topdiv_coord[0]) + topdiv_coord[1])
+
+        subgiant_indices = np.logical_and(
+            self.data[logg_col] >= topdiv_slope * (
+                self.data[teff_col] - topdiv_coord[0]) + topdiv_coord[1],
+            self.data[logg_col] < bottomdiv_slope * (
+                self.data[teff_col] - bottomdiv_coord[0]) + bottomdiv_coord[1])
+
+        dwarf_indices = (self.data[logg_col] >= bottomdiv_slope * (
+                self.data[teff_col] - bottomdiv_coord[0]) + bottomdiv_coord[1])
+
+        indexarr = [dwarf_indices, subgiant_indices, giant_indices]
+        self._setup_indices(splitnames, indexarr, crit)
+
     def subsample_len(self, namelist):
         '''Get the size of a subsample.
 
@@ -925,8 +956,6 @@ def initialize_general_APOGEE(aposplit):
         "TEFF", [4500, 5450, 5500], (
             "Too Cool", "Right Teff", "Teff Age Evolution", "Too Hot"),
         teff_crit="APOGEE Teff")
-    aposplit.split_logg("LOGG_FIT", [3.5, 4.2], ("Giant", "Subgiant", "Dwarf"),
-                           logg_crit="Subgiant Split")
     
     aposplit.split_mag(
         "H", [7, 11], ("H Bright", "H Jen", "H Faint"), mag_crit="H")
@@ -954,6 +983,7 @@ def initialize_cool_KICs(kicsplit):
         teff_crit="KIC Teff")
     kicsplit.split_logg("log(g)", 4.0, ("Jen Giant", "Jen Dwarf"),
                            logg_crit="KIC logg")
+    aposplit.split_evstate(crit="Subgiant Split")
 
     kicsplit.split_cool_dwarfs()
 
