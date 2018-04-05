426aecb617d63175d4e2db135874185dc9e5ec32
Gregory Simonian <simonian@astronomy.ohio-state.edu>
2018-04-05 09:55:54 -0400

    Added algae to biovis colors.

--- biovis_colors.py
+++ biovis_colors.py
@@ -4,12 +4,14 @@ http://mkweb.bcgsc.ca/biovis2012/'''
 
 colordict = {
     "black": (0, 0, 0), 
+    "algae": (0, 146/255.0, 146/255.0),
     "pink": (255/255.0, 109/255.0, 182/255.0), 
     "light pink": (255/255.0, 182/255.0, 119/255.0),
     "purple": (73/255.0, 0, 146/255.0), 
     "blue": (0, 109/255.0, 219/255.0), 
     "violet": (182/255.0, 109/255.0, 255/255.0), 
     "sky blue": (109/255.0, 182/255.0, 255/255.0), 
+    "light blue": (182/255.0, 219/255.0, 255/255.0),
     "red": (146/255.0, 0, 0),
     "brown": (146/255.0, 73/255.0, 0), 
     "orange": (219/255.0, 209/255.0, 0),
@@ -17,12 +19,14 @@ colordict = {
     "yellow": (255/255.0, 255/255.0, 109/255.0)}
 
 black = colordict["black"]
+algae = colordict["algae"]
 pink = colordict["pink"]
 light_pink = colordict["light pink"]
 purple = colordict["purple"]
 blue = colordict["blue"]
 violet = colordict["violet"]
 sky_blue = colordict["sky blue"]
+light_blue = colordict["light blue"]
 red = colordict["red"]
 brown = colordict["brown"]
 orange = colordict["orange"]
