58d2f0356900e8751ed810cb31b6cc39cc85dc41
Gregory Simonian <simonian@astronomy.ohio-state.edu>
2018-07-12 19:15:08 -0400

    Included interpolation over Teff and age in tracks.

--- dsep.py
+++ dsep.py
@@ -395,6 +395,145 @@ def k_grid_age_feh_alpha(teffs, age, feh, alpha, outcol="Ks", y=1, bands=1):
 #    plt.plot(10**teff_fixed, 0, 'k-')
     return kinterp_cubic(np.log10(teffs))
 
+def test_teff_k_interpolation(age, feh, alpha, outcol, y=1, bands=1):
+    '''Test temperature interpolation by removing and predicting single points.
+
+    Performs a rough test of interpolation by removing single points and
+    predicting what the value of those points ought to be.'''
+    f, (a0, a1) = plt.subplots(2, 1, gridspec_kw = {"height_ratios":[2, 1]},
+                               sharex=True)
+    afe = alpha_bin(alpha)
+    iso = DSEPIsochrone.isochrone_from_file(feh, afe, y=y, bands=bands)
+    met_table = iso.iso_dict[age]
+    restricted_table = interpolation_table_increasing_stretch(met_table)
+    teffvals, colvals = restricted_table["LogTeff"], restricted_table[outcol]
+    teff_ordered, col_ordered = ensure_array_increasing(teffvals, colvals)
+    teff_fixed, col_fixed = fix_duplicate_array_values(
+        teff_ordered, col_ordered)
+
+    # These will hold the |predicted-actual| values for each point, except the
+    # first and last.
+    linear_offtable = np.zeros(len(teff_fixed)-2)
+    cubic_offtable = np.zeros(len(teff_fixed)-2)
+    
+    # Now make interpolators with missing pieces.
+    for i, (logT, outval) in enumerate(zip(teff_fixed[1:-1], col_fixed[1:-1])):
+        mask = np.ones(len(teff_fixed), dtype="bool")
+        mask[i+1] = 0
+        assert np.count_nonzero(mask) == len(mask)-1
+        assert logT == teff_fixed[~mask][0]
+        masked_teff = teff_fixed[mask]
+        masked_out = col_fixed[mask]
+
+        kinterp_linear = interp1d(masked_teff, masked_out, kind="linear")
+        kinterp_cubic = interp1d(masked_teff, masked_out, kind="cubic")
+
+        linear_offtable[i] = np.abs(kinterp_linear(logT) - outval)
+        cubic_offtable[i] = np.abs(kinterp_cubic(logT) - outval)
+
+    # Show the interpolation on a finer grid.
+    test_teffs = np.linspace(teff_fixed[0], teff_fixed[-1], 1000)
+    kinterp_linear = interp1d(teff_fixed, col_fixed, kind="linear")
+    kinterp_cubic = interp1d(teff_fixed, col_fixed, kind="cubic")
+    test_linear = kinterp_linear(test_teffs)
+    test_cubic = kinterp_cubic(test_teffs)
+
+    # Plot the interpolation
+    hr.absmag_teff_plot(10**test_teffs, test_linear, color=bc.red, ls="-",
+                        marker="", label="Linear", axis=a0)
+    hr.absmag_teff_plot(10**test_teffs, test_cubic, color=bc.black, ls="-",
+                        marker="", label="Cubic", axis=a0)
+    hr.absmag_teff_plot(
+        10**teff_fixed, col_fixed, color=bc.blue, ls="", marker="o", axis=a0)
+    a0.set_xlabel("")
+    a0.set_ylabel("Ks")
+    a0.legend(loc="upper right")
+    a0.set_title("Age: {0:.2f} Gyr; [Fe/H]: {1:.2f}".format(age, feh))
+
+    # Plot the difference
+    a1.semilogy(10**teff_fixed[1:-1], linear_offtable, color=bc.red, ls="-",
+                marker="")
+    a1.semilogy(10**teff_fixed[1:-1], cubic_offtable, color=bc.black, ls="-",
+                marker="")
+    a1.set_xlabel("Teff (K)")
+    a1.set_ylabel("Error (mag)")
+
+def test_feh_interpolation(age, alpha, teffs, outcol, y=1, bands=1):
+    '''Plot the interpolation over [Fe/H].'''
+    f, (a0, a1) = plt.subplots(2, 1, gridspec_kw = {"height_ratios":[2, 1]},
+                               sharex=True)
+    afe = alpha_bin(alpha)
+    # Now iterate through the [Fe/H] values.
+    input_fehs = np.array([-2.5, -2, -1.5, -1, -0.5, 0.0, 0.2, 0.3, 0.5])
+    interp_fehs = np.zeros(len(input_fehs))
+    k_array = np.ma.zeros((len(input_fehs), len(teffs)))
+    for i, ifeh in enumerate(input_fehs):
+        iso = DSEPIsochrone.isochrone_from_file(ifeh, afe, y=y, bands=bands)
+        interp_fehs[i] = iso.feh
+        k_array[i,:] = interpolate_DSEP_isochrone_cols(
+            iso, age, np.log10(teffs), incol="LogTeff", outcol="Ks",
+            interp_kind="linear")
+
+    # These will hold the |predicted-actual| values for each point, except the
+    # first and last.
+    linear_offtable = np.ma.zeros((len(interp_fehs)-2, len(teffs)))
+    cubic_offtable = np.ma.zeros((len(interp_fehs)-2, len(teffs)))
+    # Now step through [Fe/H].
+    for j, (intfeh, outval) in enumerate(
+            zip(interp_fehs[1:-1], k_array[1:-1,0])):
+        mask = np.ones(len(interp_fehs), dtype="bool")
+        mask[j+1] = 0
+        fullmask = np.logical_and(mask[:,np.newaxis], np.logical_not(k_array.mask))
+        for i in range(len(teffs)):
+            masked_feh = interp_fehs[fullmask[:,i]]
+            masked_out = k_array[:,i][fullmask[:,i]]
+
+            kinterp_linear = interp1d(
+                masked_feh, masked_out, kind="linear", bounds_error=False,
+            fill_value=np.nan)
+            kinterp_cubic = interp1d(
+                masked_feh, masked_out, kind="cubic", bounds_error=False,
+                fill_value=np.nan)
+
+            linear_offtable[j,i] = np.abs(np.ma.masked_invalid(
+                kinterp_linear(intfeh)) - outval)
+            cubic_offtable[j,i] = np.abs(np.ma.masked_invalid(
+                kinterp_cubic(intfeh)) - outval)
+
+    teffindex = 0
+    # Show the interpolation on a finer grid.
+    test_fehs = np.linspace(interp_fehs[0], interp_fehs[-1], 1000)
+    masked_feh = interp_fehs[~np.ma.getmask(k_array)[:,teffindex]]
+    masked_vals = np.ma.compressed(k_array[~np.ma.getmask(k_array)[:,teffindex]])
+    kinterp_linear = interp1d(masked_feh, masked_vals, kind="linear",
+                              bounds_error=False, fill_value=np.nan)
+    kinterp_cubic = interp1d(masked_feh, masked_vals, kind="cubic",
+                             bounds_error=False, fill_value=np.nan)
+    test_linear = kinterp_linear(test_fehs)
+    test_cubic = kinterp_cubic(test_fehs)
+
+    # Plot the interpolation
+    a0.plot(
+        test_fehs, test_linear, color=bc.red, ls="-", marker="", label="Linear")
+    a0.plot(
+        test_fehs, test_cubic, color=bc.black, ls="-", marker="", label="Cubic")
+    a0.plot(
+        interp_fehs, k_array[:,teffindex], color=bc.blue, ls="", marker="o")
+    hr.invert_y_axis(a0)
+    a0.set_xlabel("")
+    a0.set_ylabel("Ks")
+    a0.legend(loc="upper right")
+    a0.set_title("Age: {0:.2f} Gyr; Teff: {1:4d} K".format(age, teffs[teffindex]))
+
+    # Plot the difference
+    a1.semilogy(interp_fehs[1:-1], linear_offtable[:,teffindex], color=bc.red, ls="-",
+                marker="")
+    a1.semilogy(interp_fehs[1:-1], cubic_offtable[:,teffindex], color=bc.black, ls="-",
+                marker="")
+    a1.set_xlabel("[Fe/H]")
+    a1.set_ylabel("Error (mag)")
+
+
 
 ###############################################################################
 # Tools needed to perform my own interpolation #
@@ -525,6 +664,40 @@ class DSEPIsochrone(object):
                     filehandle, format="ascii.fixed_width_no_header",
                     formats=formats, delimiter="")
                 filehandle.write("\n\n")
+
+def interpolate_DSEP_isochrone_cols(
+        iso, age, interp_in, incol="LogTeff", outcol="Ks",
+        interp_kind="linear", mask_outside_bounds=True):
+    '''Interpolate between the columns of an isochrone object.
+    
+    The full DSEPIsochrone object should be passed as an argument, along with
+    an age, followed by the input
+    values which should be interpolated. The columns to interpolate between
+    should be given as incol and outcol.
+    
+    The kind of interpolation to be done should be given as interp_kind, which
+    by default is linear because of the high density of points.'''
+    met_table = iso.iso_dict[age]
+    restricted_table = interpolation_table_increasing_stretch(met_table)
+    invals, outvals = restricted_table[incol], restricted_table[outcol]
+    in_ordered, out_ordered = ensure_array_increasing(invals, outvals)
+    in_fixed, out_fixed = fix_duplicate_array_values(
+        in_ordered, out_ordered)
+
+    nonmasked_in = np.ma.compressed(interp_in)
+    assert len(nonmasked_in) == len(interp_in)
+
+    if mask_outside_bounds:
+        kinterp = interp1d(
+            in_fixed, out_fixed, kind=interp_kind, fill_value=np.nan, 
+            bounds_error=False)
+        interp_out = np.ma.masked_invalid(kinterp(nonmasked_in))
+    else:
+        kinterp = interp1d(
+            in_fixed, out_fixed, kind=interp_kind, bounds_error=True)
+        interp_out = kinterp(nonmasked_in)
+
+    return interp_out
 
 def interpolate_DSEP_isochrone(newfeh, afe, y=1, bands=1):
     '''In-house interpolation of DSEP isochrones.'''
@@ -744,10 +917,6 @@ def plot_DSEP_isochrone_interpolation(teff_eval, afe=2, y=1, bands=1):
     plt.plot(interp_fehs, 10**logteffs, 'ro')
     plt.plot(interp_fehs, 10**logteff_two, 'ro')
     '''
-
-###############################################################################
-# External helper functions for the DSEP object #
-###############################################################################
 
 ###############################################################################
 # External helper functions for the DSEP object #
@@ -1514,6 +1683,12 @@ def interpolation_table_increasing_stretch(isochrone, mono_col="LogTeff"):
     first_index = np.where(coldiff >= 0)[0][0]
     # Find where the index next dips below zero.
     last_index = first_index+np.where(coldiff[first_index:] < 0)[0][0]
+    # Check for one-off blips and reinterpolate them.
+    while coldiff[last_index+1] > 0:
+        isochrone.remove_row(last_index)
+        coldiff = np.diff(isochrone[mono_col])
+        last_index = first_index+np.where(coldiff[first_index:] < 0)[0][0]
+
     return isochrone[first_index:last_index]
 
 #######################
