0fd86d45a977cec333e847cdb0d3566375dc4d69
Gregory Simonian <simonian@astronomy.ohio-state.edu>
2019-01-27 13:32:53 -0500

    Automatically generate corrections and uncertainties for ASPCAP Teff.
    The prescriptions are given in Holtzman et al (2018).

--- aspcap_corrections.py
+++ aspcap_corrections.py
@@ -1,30 +1,116 @@
 import numpy as np
 
+def flag_aspcap_giants(apogee_logg, apogee_teff):
+    '''Select the giants as classified by ASPCAP for calibration purposes.
 
-def aspcap_dwarf_teff_correction(m_h, uncor_logg, cor_logg):
+    This flags targets that ASPCAP would classify as giants and dwarfs for
+    calibration. The relations are given in Holtzman et al (2018).'''
+    giants = apogee_logg < np.minimum(2 + 2 / 1300 * (apogee_teff - 3500), 4.0)
+    return giants
+
+def correct_aspcap_teff(uncor_teff, uncor_mh, uncor_logg):
+    '''Correct raw ASPCAP temperatures to be calibrated.'''
+    giants = flag_aspcap_giants(uncor_logg, uncor_teff)
+
+    tempcors = aspcap_teff_correction(uncor_mh, giants)
+
+    corrected_temps = uncor_teff - tempcors
+    return corrected_temps
+
+def aspcap_teff_correction(uncor_mh, giants):
+    '''Calculate appropriate temperature corrections to calibrate.'''
+    temp_cors = np.zeros(len(uncor_mh))
+
+    giant_corrections = aspcap_giant_teff_correction(uncor_mh[giants])
+    dwarf_corrections = aspcap_dwarf_teff_correction(uncor_mh[~giants])
+    
+    temp_cors[giants] = giant_corrections
+    temp_cors[~giants] = dwarf_corrections
+
+    return temp_cors
+
+def aspcap_dwarf_teff_correction(m_h):
     '''Return the temperature correction for dwarfs of the given metallicity.
 
-    First there is a check of whether the uncorrected loggs are greater than or
-    less than 4.0. If greater, then a subgiant correction is used. If lower,
-    then a dwarf correction is used.'''
-    subgiant_correction = np.poly1d(np.array(
-        [ 26.09507071, -13.16135675, 36.38213935]))
+    Calculate the temperature correction as a function of metallicity as
+    reported in Holtzman et al (2018).'''
     dwarf_correction = np.poly1d(np.array(
-        [ -7.17563374, -61.47743494,  51.59026248]))
+        [ -26.0953, 13.1614, -36.3822]))
 
-    if np.any(cor_logg > 0):
-        raise ValueError("Giant calibration not included yet.")
-
-    dwarf_indices = uncor_logg <= 4.0
-    subgiant_indices = uncor_logg > 4.0
-
-    assert np.all(np.logical_or(dwarf_indices, subgiant_indices))
-
-    corrections = np.zeros(len(m_h))
-    corrections[dwarf_indices] = dwarf_correction(m_h[dwarf_indices])
-    corrections[subgiant_indices] = subgiant_correction(m_h[subgiant_indices])
+    corrections = dwarf_correction(m_h)
 
     return corrections
 
-    
+def aspcap_giant_teff_correction(m_h):
+    '''Return the temperature correction for giants of a given metallicity
 
+    Calculate the temperature correction as a function of metallicity as
+    reported in Holtzman et al (2018).'''
+    giant_correction = np.poly1d(np.array(
+        [ 7.17561, 61.4774,  -51.5903]))
+
+    corrections = giant_correction(m_h)
+
+    return corrections
+
+#############################
+# Temperature Uncertainties #
+#############################
+
+
+def calc_aspcap_teff_uncertainties(uncor_teff, sn, uncor_mh, uncor_logg):
+    '''Calculate the uncertainties in Teff.
+    
+    Use the relation in Holtzman et al (2018) to calculate the uncertainties in
+    APOGEE temperature.'''
+    giants = flag_aspcap_giants(uncor_logg, uncor_teff)
+
+    sigtemps = aspcap_teff_uncertainties(uncor_teff, sn, uncor_mh, giants)
+
+    return sigtemps
+
+def aspcap_teff_uncertainties(uncor_teff, sn, uncor_mh, giants):
+    '''Calculate appropriate temperature corrections to calibrate.'''
+    temp_sigs = np.zeros(len(uncor_teff))
+
+    giant_sigs = aspcap_giant_teff_uncertainties(
+        uncor_teff[giants], sn[giants], uncor_mh[giants])
+    dwarf_sigs = aspcap_dwarf_teff_uncertainties(
+        uncor_teff[~giants], sn[~giants], uncor_mh[~giants])
+    
+    temp_sigs[giants] = giant_sigs
+    temp_sigs[~giants] = dwarf_sigs
+
+    return temp_sigs
+
+def aspcap_dwarf_teff_uncertainties(uncor_teff, sn, uncor_mh):
+    '''Calculate the teff uncertainty for dwarfs.
+
+    Calculate the error in Teff using the dwarf relation reported in Holtzman
+    et al (2018).'''
+    Ateff = 4.583
+    Bteff = 9.000290
+    Cteff = -0.00130
+    Dteff = -0.243
+
+    logsig = (
+        Ateff + Bteff * (uncor_teff - 4500) + 
+        Cteff * (np.minimum(sn, 200) - 100) + Dteff * uncor_mh)
+    sigTeff = np.exp(logsig)
+    return sigTeff
+    
+def aspcap_giant_teff_uncertainties(uncor_teff, sn, uncor_mh):
+    '''Calculate the Teff uncertainty for giants.
+
+    Calculate the error in Teff using the giant relation reported in Holtzman
+    et al (2018).'''
+    Ateff = 4.361
+    Bteff = 0.000604
+    Cteff = -0.00196
+    Dteff = -0.0659
+
+    logsig = (
+        Ateff + Bteff * (uncor_teff - 4500) + 
+        Cteff * (np.minimum(sn, 200) - 100) + Dteff * uncor_mh)
+    sigTeff = np.exp(logsig)
+    return sigTeff
