ad5889b099e3f7cb631da15fdb3627393b371723
Gregory Simonian <simonian@astronomy.ohio-state.edu>
2019-01-27 13:34:09 -0500

    Removed STAR_BAD from the bad ASPCAP flags.
    Instead of using the STAR_BAD flag for everything, I filter on individual
    ASPCAPFLAGS, so that I can allow the VSINI_BAD flag through.

--- catalog.py
+++ catalog.py
@@ -1370,12 +1370,12 @@ def bad_ASPCAP_indices(aspcapflags, warn=False):
     '''
     badcolumns = [
         "TEFF_BAD", "LOGG_BAD", "VMICRO_BAD", "M_H_BAD", "ALPHA_M_BAD", 
-        "CHI2_BAD", "SN_BAD", "COLORTE_BAD", "ATMOS_HOLE_BAD", "ROTATION_BAD", 
+        "CHI2_BAD", "SN_BAD", "COLORTE_BAD", "ATMOS_HOLE_BAD",
         "C_M_BAD", "N_M_BAD", "NO_ASPCAP_RESULT"]
     warncolumns = [
         "TEFF_WARN", "LOGG_WARN", "VMICRO_WARN", "M_H_WARN", "ALPHA_M_WARN", 
-        "CHI2_WARN", "SN_WARN", "COLORTE_WARN", "ATMOS_HOLE_WARN", 
-        "ROTATION_WARN", "C_M_WARN", "N_M_WARN"]
+        "CHI2_WARN", "SN_WARN", "COLORTE_WARN", "ATMOS_HOLE_WARN",
+        "C_M_WARN", "N_M_WARN"]
     bad_indices = au.multi_logical_or(*[search_in_ASPCAPFLAGS(
         aspcapflags, badcol) for badcol in badcolumns])
     if warn:
